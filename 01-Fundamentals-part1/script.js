8d4726ad91d512b90d83760ff31dde8f87b41a1f
Kaique <kaiquecamposferreira@gmail.com>
2023-02-23 14:36:53 -0300

    first commit

---
+++ 01-Fundamentals-part1/script.js
@@ -0,0 +1,16 @@
+/*
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+let fisrtName = "Kaique";
+let country = "Brazil";
+let continent = "South America";
+let population = 216500000;
+
+console.log(`
+    My name is ${fisrtName} and I living in ${country}. \n
+    My country is located in ${continent} and current has aproximately 
+    ${population.toLocaleString("en-US")} inhabitants. 
+`);
